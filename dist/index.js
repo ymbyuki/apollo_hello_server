d7507743561e15590ceb1d9e4777605a775cf7b5
Yuki Yamabe <ymbyuki0104@gmail.com>
2024-05-23 11:21:18 +0900

    [FIX] tsconfig.json add:removeComments true

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,5 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
@@ -34,16 +31,10 @@ const resolvers = {
         books: () => books,
     },
 };
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
 });
